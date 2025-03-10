065d781bcaf45f26c28819481299b6aa6f0d5253
Mukund-H-Opash <mha.opash@gmail.com>
2025-03-10 12:56:44 +0530

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable all rules
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-console": "off",
+      "no-debugger": "off",
+      "react/no-unescaped-entities": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "off",
+      // You can add more rules to disable here
+    },
+  },
 ];
 
 export default eslintConfig;
